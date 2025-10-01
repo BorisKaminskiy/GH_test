5da77bfb2fc7217a198db2c5d68c072c3a8e72ad
Boris Kaminskiy <93283934+BorisKaminskiy@users.noreply.github.com>
2025-10-01 11:47:10 +0300

    Update Items.tsx

--- src/pages/Items/Items.tsx
+++ src/pages/Items/Items.tsx
@@ -3,17 +3,25 @@ import { CardContainer, Pagination } from './components'
 import cn from 'classnames'
 
 import styles from './Items.module.scss'
+import { ICardFull, ICardResults } from '~/types/card'
+import { useNavigate } from 'react-router'
+
+const onClick = (callback: (id: string, { }: { state: ICardFull })
+  => void) => (index: number, cardObject: ICardResults, id: string | number) => () => {
+
+    callback(`/project/${id}`, { state: cardObject?.items[index] as ICardFull })
+  }
 
 
 const Items: FC = () => {
+  const navigate = useNavigate()
 
   return (
     <div className={cn(styles.root)} >
-      <CardContainer />
+      <CardContainer onCardClick={onClick(navigate)} />
       <Pagination />
     </div>
   )
 }
 
 export default Items
-
